3cdec4a8de97138727b72b296b9ba3ca7142bdb5
dbunk903 <dbunk903@gmail.com>
2022-02-18 11:48:56 +0900

    Functions with Inputs

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,15 @@
-func greeting(){
-    print("hello")
+var myAge = 12
+// let var 차이도 있지만 var로 한다 해도 값만 바뀌지 타입을 바꿀 수는 없다.
+var myName: String = "MinHang"
+
+func getMilk (bottles: Int){
+    var cost = bottles * 2
+    print("cost is \(cost)")
 }
-func greetingFour(){
-    greeting()
-    greeting()
-    greeting()
-    greeting()
+
+func greeting2 (whoToGreet: String){
+    print("Hello \(whoToGreet)")
 }
+
+greeting2(whoToGreet: "Min Hang")
+getMilk(bottles: 3)

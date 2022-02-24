1eb061efa102bef943b5a69f3cb6e8467e603d9c
dbunk903 <dbunk903@gmail.com>
2022-02-24 10:41:49 +0900

    if else if &&

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,8 +1,13 @@
 func loveCalculator(){
     let ranNum = Int.random(in:0...100)
-    if ranNum == 100{
-        print("You guys love each other")
+    if ranNum > 80{
+        print("You love each other like Kanye loves Kanye.")
+    } else if ranNum >= 40 && ranNum <= 80{
+        print("You go together like Coke and Mentos.")
     } else {
-        print("You'll be forever alone")
+        print("You'll be forever alone.")
     }
 }
+loveCalculator()
+loveCalculator()
+loveCalculator()

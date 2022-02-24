624475d6595fcb9f6fd4f325dc257ce1e1e219c9
dbunk903 <dbunk903@gmail.com>
2022-02-24 11:23:55 +0900

    loveCalculator2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,7 +7,23 @@ func loveCalculator(){
     } else {
         print("You'll be forever alone.")
     }
+
 }
-loveCalculator()
-loveCalculator()
-loveCalculator()
+
+func loveCalculator2(){
+    let love = Int.random(in:0...100)
+    switch love {
+    case 80...100:
+        print("80~100")
+    case 60..<80:
+        print("60~79")
+    case ..<60:
+        print("Forever alone.")
+    default:
+        print("Never")
+    }
+    
+}
+loveCalculator2()
+loveCalculator2()
+loveCalculator2()

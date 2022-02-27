293e50f31dfa20ba06dbad1581e8f07f079b1e4d
dbunk903 <dbunk903@gmail.com>
2022-02-27 11:24:14 +0900

    Struct - method implemented

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,6 +4,11 @@ struct Town {
     let name = "Tony"
     var citizens = ["Angela", "Jack Bauer"]
     var resources = ["Grain": 100, "Ore": 42, "Wool": 75]
+    
+    //change haviour not only containing values
+    func fortify(){
+        print("Defences increased!")
+    }
 }
 
 //Initializing a Structure
@@ -15,3 +20,8 @@ print("\(myTown.name) has \(myTown.resources["Grain"]!) bags of grain.")
 // Add and Modify
 myTown.citizens.append("Keanu Reeves")
 print(myTown.citizens)
+
+// func
+myTown.fortify()
+
+

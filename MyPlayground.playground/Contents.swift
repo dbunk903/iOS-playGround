afe65d5cecc44e678d05baa17a6b43ecc3f8a141
dbunk903 <dbunk903@gmail.com>
2022-02-27 11:18:18 +0900

    Crating and Initializing with Struct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,21 +1,12 @@
 
-//Don't change this
-var studentsAndScores = ["Amy": Int(readLine()!)!, "James": Int(readLine()!)!, "Helen": Int(readLine()!)!]
-
-func highestScore(scores: [String: Int]) {
-    let a = studentsAndScores["Amy"]!
-    let b = studentsAndScores["James"]!
-    let c = studentsAndScores["Helen"]!
-    var temp = 0
-    if a > temp{
-        temp = a
-    }
-    if b > temp{
-        temp = b
-    }
-    if c > temp{
-        temp = c
-    }
-    print(temp)
+// Creating Struct
+struct Town {
+    let name = "Tony"
+    var citizens = ["Angela", "Jack Bauer"]
+    var resources = ["Grain": 100, "Ore": 42, "Wool": 75]
 }
 
+//Initializing a Structure
+
+var  myTown = Town()
+print(myTown.citizens)

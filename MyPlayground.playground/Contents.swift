bad05ecf3b094749c2e6f13ce279da6e23d9f14c
dbunk903 <dbunk903@gmail.com>
2022-03-03 11:22:12 +0900

    mutating func - modifies properties within the struct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,34 @@
-func isOdd(n : Int) -> Bool{
-    if n % 2 == 0{
-        return false
-    } else{
-        return true
+// Creating Struct
+struct Town {
+    let name: String
+    var citizens: [String]
+    var resources : [String: Int]
+    
+    init(name:String, citizens:[String], resources:[String: Int]) {
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+    func fortify(){
+        print("Defences increased!")
+    }
+    
+    mutating func harvestRice(){
+        if (resources["Rice"] != nil){
+            resources["Rice"]! += 100
+        } else {
+            resources["Rice"] = 100
+        }
     }
 }
+
+
+var anotherTown = Town(name: "Nameless Island", citizens: ["Tom Hanks"], resources: ["Coconuts": 100])
+print(anotherTown.citizens)
+anotherTown.citizens.append("Wilson")
+print(anotherTown.citizens)
+anotherTown.harvestRice()
+anotherTown.harvestRice()
+anotherTown.harvestRice()
+print(anotherTown.resources)
+

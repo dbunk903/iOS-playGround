0fdc7b0a25610c6f5cf9166e84e1c1f7e4c151bf
dbunk903 <dbunk903@gmail.com>
2022-03-10 11:48:49 +0900

    Swift 에서는 Type 검사를 매우 엄격하게 하기 떄문에 타 언어에서 되는 이런 표현들이 가능하지 않다

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,34 +1,5 @@
-// Creating Struct
-struct Town {
-    let name: String
-    var citizens: [String]
-    var resources : [String: Int]
-    
-    init(name:String, citizens:[String], resources:[String: Int]) {
-        self.name = name
-        self.citizens = citizens
-        self.resources = resources
-    }
-    func fortify(){
-        print("Defences increased!")
-    }
-    
-    mutating func harvestRice(){
-        if (resources["Rice"] != nil){
-            resources["Rice"]! += 100
-        } else {
-            resources["Rice"] = 100
-        }
-    }
+var number = 0
+if !number { // 컴파일 에러!
+  // ...
 }
-
-
-var anotherTown = Town(name: "Nameless Island", citizens: ["Tom Hanks"], resources: ["Coconuts": 100])
-print(anotherTown.citizens)
-anotherTown.citizens.append("Wilson")
-print(anotherTown.citizens)
-anotherTown.harvestRice()
-anotherTown.harvestRice()
-anotherTown.harvestRice()
-print(anotherTown.resources)
 

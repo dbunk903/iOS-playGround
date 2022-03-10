06cca4a8a0430976f4503a44aaa1eb750f06ac58
dbunk903 <dbunk903@gmail.com>
2022-03-10 11:52:17 +0900

    Swift의 switch 문은 패턴 매칭이 가능하다

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,13 @@
-var number = 0
-if !number { // 컴파일 에러!
-  // ...
+var age:Int = Int(readLine())
+var student:String = "0"
+switch age {
+case 8..<14:
+  student = "초등학생"
+case 14..<17:
+  student = "중학생"
+case 17..<20:
+  student = "고등학생"
+default:
+  student = "기타"
 }
 

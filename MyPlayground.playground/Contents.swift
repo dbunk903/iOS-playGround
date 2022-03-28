85ddfa544464a13a2da99c431d2aeb9fc2393806
dbunk903 <dbunk903@gmail.com>
2022-03-28 10:05:28 +0900

    Unary Minus Operator

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,9 @@
-var i = 0
-while i < 101{
-    print(i)
-    i += 1
-}
+let myAgeIs: Int = 31
+myAgeIs > 30 ? print("당신은 30대입니다.") : print("당신은 20대입니다.")
 
-for i in 0...100{
-    print(i)
-}
+// Prints: 당신은 20대입니다.
+
+let three = 3
+let minusThree = -three
+let plusThree = -minusThree
+print(minusThree, plusThree)

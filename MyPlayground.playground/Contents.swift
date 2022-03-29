d53be15acd737f253b7fed9dcc11fb576503eb7a
dbunk903 <dbunk903@gmail.com>
2022-03-29 13:08:35 +0900

    allowed Entry

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,3 +2,13 @@ let allowedEntry = false
 if !allowedEntry{
     print("ACCESS DENIED")
 }
+
+let enteredDoorCode = true
+let passedRetinaScan = false
+if enteredDoorCode && passedRetinaScan {
+    print("Welcome!")
+} else{
+    print("ACCESS DENIED")
+}
+
+

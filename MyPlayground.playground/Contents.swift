5a1077407f3b0e3f6df2a451632d696fc69e3562
dbunk903 <dbunk903@gmail.com>
2022-03-29 10:00:15 +0900

    Use of "Nil-Coalescing Operator"

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,6 @@
-let myAgeIs: Int = 31
-myAgeIs > 30 ? print("당신은 30대입니다.") : print("당신은 20대입니다.")
 
-// Prints: 당신은 20대입니다.
-
-let three = 3
-let minusThree = -three
-let plusThree = -minusThree
-print(minusThree, plusThree)
+let defaultColorName = "Red"
+var userDefinedColor : String?
+userDefinedColor = "green"
+var colorToUse = userDefinedColor ?? defaultColorName
+print(colorToUse)

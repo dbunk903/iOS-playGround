276a7a1c60be7983f582dfca14d8dd82fdb6d271
dbunk903 <dbunk903@gmail.com>
2022-03-29 10:03:39 +0900

    Half-Open Range Operator

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,5 @@
-for index in 1...5{
-    print("\(index) times 5 is \(index * 5)")
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+for i in 0..<count{
+    print("Person \(i + 1) is called \(names[i])")
 }

69cdd5e0aca3d04944859f97203b7c0feff5e2d7
dbunk903 <dbunk903@gmail.com>
2022-03-29 10:05:48 +0900

    One-Side Range

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,11 @@ let count = names.count
 for i in 0..<count{
     print("Person \(i + 1) is called \(names[i])")
 }
+
+for name in names[2...]{
+    print(name)
+}
+
+for name in names[...2]{
+    print(name)
+}
